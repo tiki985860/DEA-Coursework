f94d2fffbcd83283d774399b635bc00a9c3af89b
Hashini Ravinga Edandupola <87830061+HashiniEdandupola@users.noreply.github.com>
2022-09-29 00:00:03 +0530

    Update signup1.js

--- Website/SignUp/signup1.js
+++ Website/SignUp/signup1.js
@@ -1,16 +1,16 @@
 function toggleUser(){
-    document.getElementById("login-toggle").style.backgroundColor="#fff";
-     document.getElementById("login-toggle").style.color="#222";
-     document.getElementById("signup-toggle").style.backgroundColor="#57b846";
-     document.getElementById("signup-toggle").style.color="#fff";
-     document.getElementById("login-form").style.display="none";
-     document.getElementById("signup-form").style.display="block";
+    document.getElementById("staff-toggle").style.backgroundColor="#fff";
+     document.getElementById("staff-toggle").style.color="#222";
+     document.getElementById("user-toggle").style.backgroundColor="#57b846";
+     document.getElementById("user-toggle").style.color="#fff";
+     document.getElementById("staff-form").style.display="none";
+     document.getElementById("user-form").style.display="block";
  }
  
  function toggleStaff(){
-     document.getElementById("login-toggle").style.backgroundColor="#57B846";
-     document.getElementById("login-toggle").style.color="#fff";
-     document.getElementById("signup-toggle").style.backgroundColor="#fff";
-     document.getElementById("signup-toggle").style.color="#222";
-     document.getElementById("signup-form").style.display="none";
+     document.getElementById("staff-toggle").style.backgroundColor="#57B846";
+     document.getElementById("staff-toggle").style.color="#fff";
+     document.getElementById("user-toggle").style.backgroundColor="#fff";
+     document.getElementById("user-toggle").style.color="#222";
+     document.getElementById("user-form").style.display="none";
      document.getElementById("
